af3f21e90f9099b8acab276cf1c3acf043b44816
Rasmus Christophersen <r.t.christophersen@gmail.com>
2020-02-19 14:59:33 +0100

    created new scene for menus, and a canvas for each submenu. Also created a scripts MenuSwitcher and ButtonHandler (the latter will probably not be needed)

---
+++ Assets/Scripts/MenuSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwitcher : MonoBehaviour
+{
+    public GameObject uiCanvas;
+    public GameObject MainCanvas;
+    public GameObject OptionsCanvas;
+    public GameObject LevelSelectCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectThis()
+    {
+        uiCanvas.GetComponent<Canvas>().sortingLayerID = 5;
+        uiCanvas.GetComponent<Canvas>().sortingOrder = 10;
+    }
+}
