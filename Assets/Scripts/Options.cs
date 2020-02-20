8a1ac430b624fa24d78fd50e8556d33adb8b5b5f
Rasmus Christophersen <r.t.christophersen@gmail.com>
2020-02-20 11:52:15 +0100

    started work on the options menu, added some buttons and some functionality for the error highlighting toggle button, not working as of yet

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public GameObject color_button;
+    public GameObject error_button;
+    //Will need to be updated to load the playerpref once that is a thing
+    private error_Highlighting current_error = error_Highlighting.Never;
+    private enum error_Highlighting
+    {
+        Never,
+        Duplicates,
+        All
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CycleError()
+    {
+        //Not working atm
+        var valueList = Enum.GetValues(typeof(error_Highlighting)).Cast<error_Highlighting>().ToList();
+        int j = valueList.IndexOf(current_error) + 1;
+        current_error = valueList.Count == j ? (error_Highlighting)0 : (error_Highlighting)j;
+        error_button.GetComponentInChildren<Text>().text = current_error.ToString();
+    }
+}
