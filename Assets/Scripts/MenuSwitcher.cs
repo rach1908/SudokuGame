f8aa29677a9836d3cec37d2b1e554640b2003159
Rasmus Christophersen <r.t.christophersen@gmail.com>
2020-02-24 10:30:32 +0100

    MenuSwitcher.cs is now working with the new and improved method

--- Assets/Scripts/MenuSwitcher.cs
+++ Assets/Scripts/MenuSwitcher.cs
@@ -16,6 +16,7 @@ public class MenuSwitcher : MonoBehaviour
     public GameObject button_optToMain;
     public GameObject button_lvlToMain;
     private menus current_menu;
+    private List<Canvas> all_menus = new List<Canvas>();
 
     //To keep track of which menu is selected. If more menus are added, a corresponding option must be added to this Enum aswell
     
@@ -23,9 +24,15 @@ public class MenuSwitcher : MonoBehaviour
     void Start()
     {
         current_menu = menus.Main;
+        all_menus.AddRange(new List<Canvas> {
+            TutorialCanvas.GetComponent<Canvas>(),
+            MainCanvas.GetComponent<Canvas>(),
+            OptionsCanvas.GetComponent<Canvas>(),
+            LevelSelectCanvas.GetComponent<Canvas>()
+        });
         //Assigning a listener to each button that changes the menu page
         //If more menu pages are added, they must have a corresponding Gameobject above, and listener below
-        //as well as an option in the 'menus' enum.
+        //as well as an option in the 'menus' enum, the all_menus list, and the switch in ChangeMenu()
         button_lvlselect.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.LevelSelect); });
         button_options.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.Options); });
         button_HowToPlay.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.HowToPlay); });
@@ -50,6 +57,10 @@ public class MenuSwitcher : MonoBehaviour
     {
         if (current_menu != menu)
         {
+            foreach (Canvas canvas in all_menus)
+            {
+                canvas.GetComponent<Canvas>().sortingOrder = 0;
+            }
             switch (menu)
             {
                 case menus.Main:
@@ -69,7 +80,6 @@ public class MenuSwitcher : MonoBehaviour
                     break;
             }
             current_menu = menu;
-
         }
     }
 }
