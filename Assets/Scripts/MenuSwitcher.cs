3f892141e80775cfd52612e98c3ce1091af9bd3a
Rasmus Christophersen <r.t.christophersen@gmail.com>
2020-02-24 10:16:51 +0100

    options menu buttons are now working, MenuSwitcher.cs had a major rehaul, all the different button methods have been consolidated into one, also added an enum that supports said method

--- Assets/Scripts/MenuSwitcher.cs
+++ Assets/Scripts/MenuSwitcher.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuSwitcher : MonoBehaviour
 {
@@ -8,19 +9,29 @@ public class MenuSwitcher : MonoBehaviour
     public GameObject MainCanvas;
     public GameObject OptionsCanvas;
     public GameObject LevelSelectCanvas;
-    private GameObject current_canvas;
-    private List<GameObject> canvases = new List<GameObject>();
+    public GameObject button_lvlselect;
+    public GameObject button_options;
+    public GameObject button_HowToPlay;
+    public GameObject button_htpToMain;
+    public GameObject button_optToMain;
+    public GameObject button_lvlToMain;
+    private menus current_menu;
 
     //To keep track of which menu is selected. If more menus are added, a corresponding option must be added to this Enum aswell
     
     // Start is called before the first frame update
     void Start()
     {
-        current_canvas = MainCanvas;
-        canvases.Add(TutorialCanvas);
-        canvases.Add(MainCanvas);
-        canvases.Add(OptionsCanvas);
-        canvases.Add(LevelSelectCanvas);
+        current_menu = menus.Main;
+        //Assigning a listener to each button that changes the menu page
+        //If more menu pages are added, they must have a corresponding Gameobject above, and listener below
+        //as well as an option in the 'menus' enum.
+        button_lvlselect.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.LevelSelect); });
+        button_options.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.Options); });
+        button_HowToPlay.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.HowToPlay); });
+        button_htpToMain.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.Main); });
+        button_optToMain.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.Main); });
+        button_lvlToMain.GetComponent<Button>().onClick.AddListener(delegate { ChangeMenu(menus.Main); });
     }
 
     // Update is called once per frame
@@ -28,53 +39,37 @@ public class MenuSwitcher : MonoBehaviour
     {
         
     }
+    private enum menus
+    {
+        Main,
+        HowToPlay,
+        Options,
+        LevelSelect
+    }
+    private void ChangeMenu(menus menu)
+    {
+        if (current_menu != menu)
+        {
+            switch (menu)
+            {
+                case menus.Main:
+                    MainCanvas.GetComponent<Canvas>().sortingOrder = 1;
+                    break;
+                case menus.HowToPlay:
+                    TutorialCanvas.GetComponent<Canvas>().sortingOrder = 1;
+                    break;
+                case menus.Options:
+                    OptionsCanvas.GetComponent<Canvas>().sortingOrder = 1;
+                    break;
+                case menus.LevelSelect:
+                    LevelSelectCanvas.GetComponent<Canvas>().sortingOrder = 1;
+                    break;
+                default:
+                    Debug.Log("An option in the 'menus' enum does not have a corresponding option in the switch in ChangeMenu() - MenuSwitcher.cs");
+                    break;
+            }
+            current_menu = menu;
 
-    public void ChangeMenu_Main()
-    {
-        if (current_canvas != MainCanvas)
-        {
-            current_canvas = MainCanvas;
-            foreach (GameObject obj in canvases)
-            {
-                obj.GetComponent<Canvas>().sortingOrder = 0;
-            }
-            MainCanvas.GetComponent<Canvas>().sortingOrder = 1;
-        }
-    }
-    public void ChangeMenu_Tutorial()
-    {
-        if (current_canvas != TutorialCanvas)
-        {
-            current_canvas = TutorialCanvas;
-            foreach (GameObject obj in canvases)
-            {
-                obj.GetComponent<Canvas>().sortingOrder = 0;
-            }
-            TutorialCanvas.GetComponent<Canvas>().sortingOrder = 1;
-        }
-    }
-    public void ChangeMenu_Options()
-    {
-        if (current_canvas != OptionsCanvas)
-        {
-            current_canvas = OptionsCanvas;
-            foreach (GameObject obj in canvases)
-            {
-                obj.GetComponent<Canvas>().sortingOrder = 0;
-            }
-            OptionsCanvas.GetComponent<Canvas>().sortingOrder = 1;
-        }
-    }
-    public void ChangeMenu_LevelSelect()
-    {
-        if (current_canvas != LevelSelectCanvas)
-        {
-            current_canvas = LevelSelectCanvas;
-            foreach (GameObject obj in canvases)
-            {
-                obj.GetComponent<Canvas>().sortingOrder = 0;
-            }
-            LevelSelectCanvas.GetComponent<Canvas>().sortingOrder = 1;
         }
     }
 }
--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -7,20 +7,31 @@ using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
-    public GameObject color_button;
+    public GameObject color_button01;
+    public GameObject color_button02;
+    public GameObject color_button03;
     public GameObject error_button;
     //Will need to be updated to load the playerpref once that is a thing
-    private error_Highlighting current_error = error_Highlighting.Never;
+    private error_Highlighting current_error;
     private enum error_Highlighting
     {
         Never,
         Duplicates,
         All
     }
+    private enum color_Themes
+    {
+        Yellow,
+        Blue,
+        Grey
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        current_error = error_Highlighting.Never;
+        color_button01.GetComponent<Button>().onClick.AddListener(delegate { ColorButtons(color_Themes.Yellow); });
+        color_button02.GetComponent<Button>().onClick.AddListener(delegate { ColorButtons(color_Themes.Blue); });
+        color_button03.GetComponent<Button>().onClick.AddListener(delegate { ColorButtons(color_Themes.Grey); });
     }
 
     // Update is called once per frame
@@ -31,10 +42,15 @@ public class Options : MonoBehaviour
 
     public void CycleError()
     {
-        //Not working atm
-        var valueList = Enum.GetValues(typeof(error_Highlighting)).Cast<error_Highlighting>().ToList();
+        List<Options.error_Highlighting> valueList = Enum.GetValues(typeof(error_Highlighting)).Cast<Options.error_Highlighting>().ToList();
         int j = valueList.IndexOf(current_error) + 1;
         current_error = valueList.Count == j ? (error_Highlighting)0 : (error_Highlighting)j;
         error_button.GetComponentInChildren<Text>().text = current_error.ToString();
+        //Need to add player pref?
+    }
+
+    private void ColorButtons(color_Themes color)
+    {
+
     }
 }
